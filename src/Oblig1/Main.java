bfec57a27bd40cb25c9fcdc48abee5cfe451c3f8
Oskar <oskar_ruyter@hotmail.com>
2020-09-08 14:24:12 +0200

    nesten ferdig nå gutta

--- src/Oblig1/Main.java
+++ src/Oblig1/Main.java
@@ -5,20 +5,24 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hei");
-        // dette er en test
 
         int[] a = Oblig1.randPerm(10);
-        System.out.println(Oblig1.ombyttinger(a));
+        System.out.println("antall ombyttinger : "+Oblig1.ombyttinger(a));
+        System.out.println("gjennomsnittlig ombyttinger : "+ helperClass.avgAntallOmbyttinger());
+        System.out.println(Oblig1.antallUlikeSortert(new int[]{1,2,2,3,5,5,5,5,9}));
 
-       // System.out.println(Oblig1.antallUlikeSortert(new int[]{1,2,2,3,5,5,5,5,9}));
 
-
-      //  int[] a = new int[]{1, 2, 3, 11, 5, 6, 7, 8, 9};
-        //int m = Oblig1.maks(a);
-        //System.out.println("Støste tallet er " + m);
+        int m = Oblig1.maks(a);
+        System.out.println("Storste tallet er " + m);
         System.out.println("antall ulike usortert : "+Oblig1.antallUlikeUsortert(new int[] {5, 3, 7, 4, 3, 5, 7, 8, 6, 7}));
-        Oblig1.delsortering(new int[] {6, 10, 9, 4, 1, 3, 8, 5, 2, 7});
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {6, 10, 9, 4, 1, 3, 8, 5, 2, 7})));
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {6,10,4,2,8})));
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {3, 11, 7, 5, 1, 17})));
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {-4, -1, 3, 0, 2, -3, -2, 4, 1})));
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {-1, -3, -7, -5, -17, -11})));
+        System.out.println(Arrays.toString(Oblig1.delsortering(new int[] {-10, -6, -8, -2, -12})));
+
+
 
         char[] c = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
         Oblig1.rotasjon(c);
@@ -28,17 +32,20 @@ public class Main {
 
         char[] k = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
         System.out.println(Arrays.toString(k) + " Originaltabellen");
-        Oblig1.rotasjon(k,3); System.out.println(Arrays.toString(k) + " En rotasjon på tre enheter mot høyre");
+        Oblig1.rotasjon(k,3); System.out.println(Arrays.toString(k) + " En rotasjon på tre enheter mot hoyre");
         Oblig1.rotasjon(k,-2); System.out.println(Arrays.toString(k) + " En rotasjon på to enheter mot venstre");
 
-        System.out.println(Arrays.toString(Oblig1.indekssortering(new int[] {6,10,16,11,7,12,3,9,8,5})));
+        System.out.println("Oppgave 8 : "+Arrays.toString(Oblig1.indekssortering(new int[] {6,10,3,16,11,7,12,3,9,8,5})));
 
 
 
         //Tester oppgave 9
         int[] array = Oblig1.randPerm(20);
+
         int[] treminste = Oblig1.tredjeMin(array);
         System.out.print("Oppgave 9 :\nInput array: " + Arrays.toString(array) + "\nIndex av tre minste verdier: " + Arrays.toString(treminste) + "\nVerdi av tre minste verdier: " + array[treminste[0]] + ", " + array[treminste[1]] + ", " + array[treminste[2]] + "\n");
+
+
 
         String flett1 = "ABC";
         String flett2 = "DEFGH";
@@ -51,7 +58,6 @@ public class Main {
         Oblig1.flett(flett5, flett6);
 
         //"AM "​,​"L"​,​"GEDS"​,​"ORATKRR"​,​""​,​"R TRTE"​,​"IO"​,​"TGAUU"​
-        //System.out.println(Oblig1.flett("AM ", "L", "GEDS", "ORATKRR", "", "R TRTE", "IO", "TGAUU"));
         System.out.println(Oblig1.flett("AM ", "L", "GEDS", "ORATKRR", "", "R TRTE", "IO", "TGAUU"));
 
 
--- src/Oblig1/Oblig1.java
+++ src/Oblig1/Oblig1.java
@@ -1,22 +1,28 @@
+/*
+s333993 - Oskar Ruyter
+s344170 - Marius Sørum
+s331179 - Simen Nordgaard Renberg
+s344179 - Kunta Samuel Houen
+*/
+
 package Oblig1;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
-public class Oblig1 {
+class Oblig1 {
     private Oblig1() {
     }
 
     ///// Oppgave 1 //////////////////////////////////////
-    public static int maks(int[] a) {
-        int størsteTall = 0;
+    static int maks(int[] a) {
+        int storsteTall = 0;
 
         if (a.length < 1) {
             throw new NoSuchElementException("Tabellen er tom!");
         } else if (a.length == 1) {
-            størsteTall = a[0];
+            storsteTall = a[0];
         }
 
         for (int i = 1; i < a.length; i++) {
@@ -26,16 +32,16 @@ public class Oblig1 {
                 a[i - 1] = temp;
             }
 
-            størsteTall = a[i];
+            storsteTall = a[i];
         }
-        return størsteTall;
+        return storsteTall;
     }
 
 
-    // Når blir det flest ombyttinger?
-    // Når blir det færrest?
+    // Naar blir det flest ombyttinger?
+    // Naar blir det faerrest?
     // Hvor mange blir det i gjennomsnitt?
-    public static int ombyttinger(int[] a) {
+    static int ombyttinger(int[] a) {
         int antall = 0;
         //12, 1, 2, 3, 11, 5, 6, 7, 8, 9
         for (int i = 1; i < a.length; i++) {
@@ -44,27 +50,26 @@ public class Oblig1 {
                 int temp = a[i];
                 a[i] = a[i - 1];
                 a[i - 1] = temp;
-              //  System.out.println(Arrays.toString(a));
             }
         }
         return antall;
     }
 
     //----------------------------------------------------------------------------------------------------
-    public static void bytt(int[] a, int i, int j){
+    private static void bytt(int[] a, int i, int j){
 
         int temp = a[i]; a[i] = a[j]; a[j] = temp;
     }
-    public static int[] randPerm(int n){  // en effektiv versjon
+    static int[] randPerm(int n){  // en effektiv versjon
 
         Random r = new Random();         // en randomgenerator
         int[] a = new int[n];            // en tabell med plass til n tall
 
         Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
 
-        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        for (int k = n - 1; k > 0; k--)  // loop som går n - 1 ganger
         {
-            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            int i = r.nextInt(k+1);        // et tilfeldig tall fra 0 til k
             bytt(a,k,i);                   // bytter om
         }
 
@@ -73,7 +78,7 @@ public class Oblig1 {
 
 
     ///// Oppgave 2 //////////////////////////////////////
-    public static int antallUlikeSortert(int[] a) {
+    static int antallUlikeSortert(int[] a) {
         if (a.length < 1) return 0;
         int begin = 0;
         int end = a.length;
@@ -95,7 +100,7 @@ public class Oblig1 {
     }
 
     ///// Oppgave 3 //////////////////////////////////////
-    public static int antallUlikeUsortert(int[] a) {
+    static int antallUlikeUsortert(int[] a) {
         if (a.length < 1) return 0;
         int begin = 0;
         int end = a.length;
@@ -103,17 +108,17 @@ public class Oblig1 {
         int antallUlike = 1;
         boolean funnetTidligere = false;
 
-        // Løper gjennom listen fra index 1 og sammenligner a[i] med a[-1], hvis de er forskjellige går den inn i neste løkke
+        // Looper gjennom listen fra index 1 og sammenligner a[i] med a[-1], hvis de er forskjellige går den inn i neste løkke
         for (int i = begin+1; i < end; i++) {
             if (a[i] != temp) {
                 // lagrer verdien i hjelpe variabel
                 int helper = a[i];
-                // løper gjennom listen fra der den fant et tall som var forskjellig og sjekker alle tallene forran om dette tallet finnes fra før, ved hjelp av en for-løkke som teller bakover
+                // looper gjennom listen fra der den fant et tall som var forskjellig og sjekker alle tallene forran om dette tallet finnes fra for, ved hjelp av en for-loop som teller bakover
                 for (int j = i-1; j >= begin; j--) {
                     if (helper == a[j]) { // hvis tallet er funnet fra før så gå ut av loopen
                         funnetTidligere = true;
                         break;
-                    } else if(helper != a[j]) { //hvis tallet ikke er funnet fra før, setter bool'en til false og plusser senere 1 på antallUlike
+                    } else if(helper != a[j]) { //hvis tallet ikke er funnet tidligere, setter bool'en til false og plusser senere 1 på antallUlike
                         funnetTidligere = false;
                     }
                 }
@@ -127,70 +132,50 @@ public class Oblig1 {
     }
 
     ///// Oppgave 4 //////////////////////////////////////
-    // 6, 10, 9, 4, 1, 3, 8, 5, 2, 7
-    public static int[] delsortering(int[] a) {
-
-        if (a.length == 0) {
+    static int[] delsortering(int[] a) {
+        if (a.length < 1) {
             return a;
         }
         int begin = 0;
         int end = a.length;
-
         int left = 0;
         int right = end-1;
-        int partall = 0;
-        int oddetall = 0;
         boolean harOddetall = false;
         boolean harPartall = false;
         // sjekker om array'et inneholder kun partall, kun oddetall, eller en blanding. Resultatet av dette bruks senere i beslutningen om hvordan listen skal sorteres
         for (int i = begin; i < end; i++) {
             if (a[i] % 2 == 0) {
                 harPartall = true;
-            } else if (a[i] % 2 == 1) {
+            } else if (a[i] % 2 == 1 || a[i] % 2 == -1) { // må her sjekke på om resten baade blir 1 for positive tall og -1 for negative tall
                 harOddetall = true;
             }
         }
-
-        System.out.println(Arrays.toString(a));
-        while(left < right) {
-
-            while (a[left] % 2 == 1 && left < right) {
-                System.out.println("Left : "+left);
+        // behover kun aa gaa inn i denne lokken dersom arrayet inneholder baade partall og oddetall,
+        // dersom det kun er en av delene saa hoppes denne koden over og gaar rett til sortering
+        while(left < right && (harOddetall && harPartall)) {
+            while ((a[left] % 2 == 1 || a[left] % 2 == -1) && left < right) { // maa her sjekke paa om resten baade blir 1 for positive tall og -1 for negative tall
                 left++;
-                if (a[left] < 0) {
-                    left++;
-                }
             }
             while (a[right] % 2 == 0 && right > left) {
-                System.out.println("Right : "+right);
                 right--;
-                if (a[right] < 0) {
-                    right--;
-                }
             }
-            if (left >= right) break;
             int temp = a[left];
             a[left] = a[right];
             a[right] = temp;
-
-            System.out.println(Arrays.toString(a));
-            //sortere oddetallene fra index 0 til left (4)
         }
-        System.out.println(harPartall+"-------------"+harOddetall);
+
         // dersom listen inneholder kun partall, eller kun oddetall sorteres hele listen en gang
-        if ((harPartall && !harOddetall) || (harOddetall && !harPartall)) {
+        if (!harPartall || !harOddetall) {
             helperClass.quicksort(a, 0, end-1);
-        } else { // dersom listen inneholder en blanding av partall og oddetall sorteres listen to ganger, først venstre halvdel(oddetall), deretter høyre halvdel(partall)
+        } else { // dersom listen inneholder en blanding av partall og oddetall sorteres listen to ganger, forst venstre halvdel(oddetall), deretter hoyre halvdel(partall)
             helperClass.quicksort(a, 0, right-1);
             helperClass.quicksort(a, right, end-1);
         }
-
         return a;
-       // System.out.println("etter quicksort : "+Arrays.toString(a));
     }
 
     ///// Oppgave 5 //////////////////////////////////////
-    public static void rotasjon(char[] a) {
+    static void rotasjon(char[] a) {
         if (a.length >= 2) {
             char c = a[a.length - 1];
             for(int i = a.length - 1; i >= 1; --i) {
@@ -202,7 +187,7 @@ public class Oblig1 {
 
 
     ///// Oppgave 6 //////////////////////////////////////
-    public static void rotasjon(char[] a, int k) {
+    static void rotasjon(char[] a, int k) {
             int n = a.length;
             if (n < 2){
                 return;
@@ -220,7 +205,7 @@ public class Oblig1 {
 
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
-    public static String flett(String s, String t) {
+    static String flett(String s, String t) {
         char[] a = s.toCharArray();
         char[] b = t.toCharArray();
         char[] c = new char[a.length + b.length];
@@ -253,8 +238,8 @@ public class Oblig1 {
     }
 
     /// 7b)
-    public static String flett(String... s) {
-        //hvilken indeks vi er interesserte i (den første bokstaven i arrayet, den andre bokstaven osv)
+    static String flett(String... s) {
+        //hvilken indeks vi er interesserte i (den forste bokstaven i arrayet, den andre bokstaven osv)
         int relevantCharPosition = 0;
 
         //antall arrays vi har hentet alle karakterene fra
@@ -263,8 +248,8 @@ public class Oblig1 {
 
         //mens det fortsatt finnes minst ett array med bokstaver vi skal hente ut
         while (numberOfFinishedArrays < s.length){
-            for (int i = 0; i < s.length; i++) {
-                char[] currentChars = s[i].toCharArray();
+            for (String value : s) {
+                char[] currentChars = value.toCharArray();
                 if (currentChars.length > relevantCharPosition) {
                     numberOfFinishedArrays = 0;
                     out.append(currentChars[relevantCharPosition]);
@@ -280,8 +265,7 @@ public class Oblig1 {
 
 
     ///// Oppgave 8 //////////////////////////////////////
-    // 6,10,16,11,7,12,3,9,8,5
-    public static int[] indekssortering(int[] a) {
+    static int[] indekssortering(int[] a) {
         int[] c = new int[a.length];
         System.arraycopy(a, 0, c, 0, a.length);
         Arrays.sort(c);
@@ -293,14 +277,15 @@ public class Oblig1 {
                 }
             }
         }
+        System.out.println(Arrays.toString(b));
         return b;
     }
 
     ///// Oppgave 9 //////////////////////////////////////
-    public static int[] tredjeMin(int[] a) {
+    static int[] tredjeMin(int[] a) {
         int n = a.length;     // tabellens lengde
-        if (n < 3) {       // må ha minst tre verdier
-            throw new NoSuchElementException("Det er for få verdier i tabellen. Du må ha minst tre verdier");
+        if (n < 3) {       // maa ha minst tre verdier
+            throw new NoSuchElementException("Det er for lite verdier i tabellen. Du maa ha minst tre verdier");
         }
 
         int[] c = indekssortering(new int[] {a[0], a[1],a[2]});
@@ -346,11 +331,11 @@ public class Oblig1 {
     }
 
     ///// Oppgave 10 //////////////////////////////////////
-    public static int bokstavNr(char bokstav) {
+    static int bokstavNr(char bokstav) {
         throw new UnsupportedOperationException();
     }
 
-    public static boolean inneholdt(String a, String b) {
+    static boolean inneholdt(String a, String b) {
             char [] chars = a.toCharArray();
             char [] chars1 = b.toCharArray();
 
@@ -358,7 +343,7 @@ public class Oblig1 {
                 return false;
             }
 
-            int[] Aarr = new int[256], Barr = new int[256];  // hjelpetabell med lenge 256, virker også hvis a og b inneholder andre tegn
+            int[] Aarr = new int[256], Barr = new int[256];  // hjelpetabell med lenge 256, virker ogsaa hvis a og b inneholder andre tegn
 
              for (char c : chars) Aarr[c]++;     // teller opp tegnene i a
              for (char c : chars1) Barr[c]++;     // teller opp tegnene i b
@@ -374,7 +359,7 @@ public class Oblig1 {
 
 
 class helperClass {
-    public static void quicksort(int[] a, int begin, int end) {
+    static void quicksort(int[] a, int begin, int end) {
         if (begin < end) {
             int p = partisjoner(a, begin, end);
             quicksort(a, begin, p-1);
@@ -382,16 +367,7 @@ class helperClass {
         }
     }
 
-    public static int byttPivotBakerst(int[] a, int begin, int end) {
-        int p = a[end/2];
-        int temp = a[end];
-        a[end] = p;
-        a[end/2] = temp;
-        System.out.println(Arrays.toString(a));
-        return p;
-    }
-
-    public static int partisjoner(int[] a, int begin, int end) {
+    private static int partisjoner(int[] a, int begin, int end) {
         int pivot = a[end];
         int j = begin-1;
         for (int i = begin; i < end; i++) {
@@ -408,9 +384,14 @@ class helperClass {
         return j+1;
     }
 
-    public static void byttPivotTilbake(int[] a, int end, int q) {
-        int temp = a[end];
-        a[end] = a[q];
-        a[q] = temp;
+    static double avgAntallOmbyttinger() {
+        double avg;
+        int ombyttinger = 0;
+        for (int i = 0; i<10; i++) {
+            int[] a = Oblig1.randPerm(50);
+            ombyttinger += Oblig1.ombyttinger(a);
+        }
+        avg = ombyttinger/10;
+        return avg;
     }
 }
